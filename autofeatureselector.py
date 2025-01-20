2997b3d9568dc0f3357a6494daa94cb9a4503830
Lev Byzalov <lbyzalov@gmail.com>
2025-01-19 20:05:35 -0500

    Added chi-squared selection

--- autofeatureselector.py
+++ autofeatureselector.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import sklearn as sk
 
+# uses pearson correlation to select features
 def cor_selector(X, y, num_feats):
     # list of features
     feats = X.keys().tolist()
@@ -20,3 +21,15 @@ def cor_selector(X, y, num_feats):
     corr_support = [True if i in best_feat else False for i in feats]
     return corr_support, best_feat
 
+# uses chi-squared correlateion to select features
+def chi_squared_selector(X, y, num_feats):
+    # creates chi-sq. selector function and trains it on normalized data
+    chi_sel = sk.feature_selection.SelectKBest(sk.feature_selectionchi2, k=num_feats)
+    x_norm = sk.preprocessing.MinMaxScaler().fit_transform(X)
+    chi_sel.fit(x_norm, y)
+
+    # gets num_feats best columns from chi_sel
+    chi_support = chi_sel.get_support()
+    chi_feat = X.loc[:,chi_support].columns.to_list()
+    
+    return chi_support, chi_feat
